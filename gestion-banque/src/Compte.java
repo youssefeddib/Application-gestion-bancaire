819cc0cfac017175a2d42d89f5b59756e13800c6
youssef eddib <eddiby79@gmail.com>
2024-12-25 15:12:49 +0100

    creer classes

---
+++ gestion-banque/src/Compte.java
@@ -0,0 +1,39 @@
+public class Compte {
+
+    private int nmcompte;
+    private int solde;
+    private String client;
+
+public Compte(int nmcompte, int solde, String client, String methode){
+    this.nmcompte = nmcompte;
+    this.solde = solde;
+    this.client = client;
+
+}
+
+    public int getNmcompte() {
+        return nmcompte;
+    }
+
+    public void setNmcompte(int nmcompte) {
+        this.nmcompte = nmcompte;
+    }
+
+    public int getSolde() {
+        return solde;
+    }
+
+    public void setSolde(int solde) {
+        this.solde = solde;
+    }
+
+    public String getClient() {
+        return client;
+    }
+
+    public void setClient(String client) {
+        this.client = client;
+    }
+}
+
+
